a938e4a24637f7702f90291fd40f0e70536b4b84
HafidTraikzi <hafid.traikzi@gmail.com>
2015-06-19 18:14:13 +0200

    Add alert lunch message for each day

--- slackbot/slack-memo-inbox.rb
+++ slackbot/slack-memo-inbox.rb
@@ -6,19 +6,23 @@
 class SlackMemo
 	attr_accessor :team_odd_weeks
 	attr_accessor :team_even_weeks
+	attr_accessor :messages9
+	attr_accessor :messages11
 
-	def initialize(team1, team2)
-		@slack_channel = '#team'
+	def initialize(team1, team2, message9, message11)
+		@slack_channel = '#testdev'
 		@slack_username = "inbox-bot"
 		@slack_emoji = ":ghost:"
 		@slack_url = "https://hooks.slack.com/services/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"
 		
 		@team_odd_weeks = team1
 		@team_even_weeks = team2
+		@messages9 = message9
+		@messages11 = message11
 	end
 
 	def memo_today_user
-		index = self.get_day - 1 #days start at 1 but array at 0
+		index = self.is_day - 1 #days start at 1 but array at 0
 
 		if(self.is_odd_week?)
 			user = @team_odd_weeks[index]
@@ -27,19 +31,28 @@ class SlackMemo
 		end
 
 		self.send_message_to_slack(user + ' petit rappel pour inbox!')
-
 	end
-
-	def get_day
-		return Time.now.strftime('%u').to_i
+	
+	def memo_lunch_user
+    if ARGV[0] == "msg1"
+      self.send_message_to_slack("@group" + " " + @messages9[is_day - 1])
+    end
+    
+    if ARGV[0] == "msg2"
+      self.send_message_to_slack("@group" + " " + @messages11[is_day - 1])
+    end
+	end
+	
+	def is_day
+	  return Time.now.wday
 	end
 
 	def is_odd_week?
-		return Time.now.strftime('%W').to_i.odd?
+		return Time.now.wday.odd?
 	end
 
 	def is_even_week?
-		return Time.now.strftime('%W').to_i.even?
+		return Time.now.wday.even?
 	end
 
 	def send_message_to_slack(message)
@@ -55,10 +68,13 @@ end
 #INIT
 team1 = ["@fred","@elise","@renversou","@nicolas","@laurepichot"]
 team2 = ["@charlotte","@zemaud","@camille","@tony.tiratay","@acoriol"]
+message9 = ["petit rappel pour le lunch à 13h! Pensez à preparez vos paniers repas!", "aaaa", "bbbb", "cccc!", "dddd", "eeee", "ffff"]
+message11 = ["petit rappel pour le lunch à 13h! La réunion approche!", "aaaa", "bbbb", "cccc!", "dddd", "eeee", "ffff"]
 
 
 #MAIN 
 
-memo = SlackMemo.new(team1,team2);
+memo = SlackMemo.new(team1,team2,message9,message11);
 
 memo.memo_today_user();
+memo.memo_lunch_user();
